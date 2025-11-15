9c7dc59f49814c0f59afb67c3e2767a370fd5044
Maika <guintocaleb.com>
2025-11-15 19:09:30 +0800

    Apply .gitignore

--- StudentPerformanceTracker.Services/obj/Debug/net9.0/StudentPerformanceTracker.Services.AssemblyInfo.cs
+++ StudentPerformanceTracker.Services/obj/Debug/net9.0/StudentPerformanceTracker.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPerformanceTracker.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ae42b6c34c77f46657015fa2cd98980ede5fc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3ea144395e289331827b053be06935c950feb43")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPerformanceTracker.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPerformanceTracker.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
